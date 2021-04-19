f389f1ed8f00f7e7ff0bb3905aef235123e2ae41
usu2 name <usu2@example.com>
2021-04-19 16:37:37 +0200

    Validacion en cambio1

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
 	public static void main(String arg) {
 		System.out.println("Hola mundo");
 		System.out.println("Hola mundo");
+		System.out.println("Hola mundo desde la rama Cambio1");
 	}
 }
